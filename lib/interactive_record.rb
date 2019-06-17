854a05f75dd0b7b6f838184c96470fe5545751e1
Haley DeKeyser <hcd09@live.com>
2019-06-17 18:13:20 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,7 +48,10 @@ class InteractiveRecord
       INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
       VALUES (#{values_for_insert})
     SQL
+    
     DB[:conn].execute(sql)
+    
+    @id = "SELECT "
   end
   
 end

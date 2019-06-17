ab8b874c15cad1347845efcd235320b6a3032940
Haley DeKeyser <hcd09@live.com>
2019-06-17 18:23:21 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,7 @@ class InteractiveRecord
   end
   
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
   end
   
 end

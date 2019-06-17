7ee1c99ca642edb378782447784510df12d56fb7
Haley DeKeyser <hcd09@live.com>
2019-06-17 17:49:39 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,6 +10,6 @@ class InteractiveRecord
   def self.column_names
     DB[:conn].results_as_hash = true
     
-    sql = PRAGMA table info 
+    sql = "PRAGMA table_info '#{table_name}'"
   end
 end

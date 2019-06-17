0da7284bf6ce03773336db3d3274efea1ade0817
Haley DeKeyser <hcd09@live.com>
2019-06-17 18:40:49 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,8 +55,11 @@ class InteractiveRecord
   end
   
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    sql = "SELECT * FROM #{table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
+  
+  def self.find_by(attribute)
+    sql = "SELECT * FROM #{table_name} WHERE "
   
 end

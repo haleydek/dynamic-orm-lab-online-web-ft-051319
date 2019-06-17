e47787fc00da22e2b30dc59f6a9c72bd4d8f5418
Haley DeKeyser <hcd09@live.com>
2019-06-17 18:38:48 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -32,6 +32,7 @@ class InteractiveRecord
   
   def col_names_for_insert
     self.class.column_names.delete_if { |col_name| col_name == "id" }.join(", ")
+    binding.pry
   end
   
   def values_for_insert
